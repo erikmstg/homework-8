8cded119850eb69b0060ba8041435186f89d0b3b
Erik Mikael <erikfreecss@gmail.com>
2023-02-24 15:16:25 +0700

    update seed

--- seeding/index.js
+++ seeding/index.js
@@ -3,8 +3,8 @@ const fs = require("fs");
 
 const seedQuery = fs.readFileSync("seeding/seeding.sql", { encoding: "utf8" });
 
-pool.query(seedQuery, (err, result) => {
-  if (err) throw err.stack;
+pool.query(seedQuery, null, (err, result) => {
+  if (err) throw err;
 
   console.log(result);
   console.log("seeding complete...");
